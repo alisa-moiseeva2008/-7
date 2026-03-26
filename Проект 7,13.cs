2c6d66c68d1f310aa0591ae43de8af3c296d2454
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:33:00 +0400

    Update Проект 7,13.cs

--- Проект 7,13.cs
+++ Проект 7,13.cs
@@ -1,45 +1,43 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Random rnd = new Random();
+        int[,] матрица = new int[8, 5];
+
+        for (int i = 0; i < 8; i++)
         {
-            Random rnd = new Random();
-            int[,] матрица = new int[8, 5];
-            
-            for (int i = 0; i < 8; i++)
+            for (int j = 0; j < 5; j++)
             {
-                for (int j = 0; j < 5; j++)
-                {
-                    матрица[i, j] = rnd.Next(20, 41);
-                }
+                матрица[i, j] = rnd.Next(20, 41);
             }
-            Console.WriteLine("Матрица 8x5:");
-            for (int i = 0; i < 8; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    Console.Write(матрица[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("\nСтроки, содержащие не более 3-х четных чисел:");
-            for (int i = 0; i < 8; i++)
-            {
-                int четные = 0;
-                for (int j = 0; j < 5; j++)
-                {
-                    if (матрица[i, j] % 2 == 0)
-                    {
-                        четные++;
-                    }
-                }
-                if (четные <= 3)
-                {
-                    Console.WriteLine($"Строка {i + 1}: {четные} четных чисел");
-                }
-            }
-            Console.ReadKey();
         }
+        Console.WriteLine("Матрица 8x5:");
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write(матрица[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("\nСтроки, содержащие не более 3-х четных чисел:");
+        for (int i = 0; i < 8; i++)
+        {
+            int четные = 0;
+            for (int j = 0; j < 5; j++)
+            {
+                if (матрица[i, j] % 2 == 0)
+                {
+                    четные++;
+                }
+            }
+            if (четные <= 3)
+            {
+                Console.WriteLine($"Строка {i + 1}: {четные} четных чисел");
+            }
+        }
+        Console.ReadKey();
     }
 }
