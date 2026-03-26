81738217a176ab683fb76b129397ad4ec9e17d7a
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:40:45 +0400

    Update Проект 7,6.cs

--- Проект 7,6.cs
+++ Проект 7,6.cs
@@ -1,46 +1,44 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Random rnd = new Random();
+        int[,] матрица = new int[5, 10];
+
+        for (int i = 0; i < 5; i++)
         {
-            Random rnd = new Random();
-            int[,] матрица = new int[5, 10];
-            
-            for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 10; j++)
             {
-                for (int j = 0; j < 10; j++)
-                {
-                    матрица[i, j] = rnd.Next(-20, 31);
-                }
+                матрица[i, j] = rnd.Next(-20, 31);
             }
-            Console.WriteLine("Матрица 5x10:");
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    Console.Write(матрица[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("\nСтроки, содержащие только положительные элементы:");
-            for (int i = 0; i < 5; i++)
-            {
-                bool всеПоложительные = true;
-                for (int j = 0; j < 10; j++)
-                {
-                    if (матрица[i, j] <= 0)
-                    {
-                        всеПоложительные = false;
-                        break;
-                    }
-                }
-                if (всеПоложительные)
-                {
-                    Console.Write((i + 1) + " ");
-                }
-            }
-            Console.ReadKey();
         }
+        Console.WriteLine("Матрица 5x10:");
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write(матрица[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("\nСтроки, содержащие только положительные элементы:");
+        for (int i = 0; i < 5; i++)
+        {
+            bool всеПоложительные = true;
+            for (int j = 0; j < 10; j++)
+            {
+                if (матрица[i, j] <= 0)
+                {
+                    всеПоложительные = false;
+                    break;
+                }
+            }
+            if (всеПоложительные)
+            {
+                Console.Write((i + 1) + " ");
+            }
+        }
+        Console.ReadKey();
     }
 }
