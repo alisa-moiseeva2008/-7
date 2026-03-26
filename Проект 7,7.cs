8ef5eca195a2df8fda5fdebd90df60191028a502
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:41:16 +0400

    Update Проект 7,7.cs

--- Проект 7,7.cs
+++ Проект 7,7.cs
@@ -1,46 +1,44 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Random rnd = new Random();
+        int[,] матрица = new int[7, 4];
+
+        for (int i = 0; i < 7; i++)
         {
-            Random rnd = new Random();
-            int[,] матрица = new int[7, 4];
-            
-            for (int i = 0; i < 7; i++)
+            for (int j = 0; j < 4; j++)
             {
-                for (int j = 0; j < 4; j++)
-                {
-                    матрица[i, j] = rnd.Next(-30, 31);
-                }
+                матрица[i, j] = rnd.Next(-30, 31);
             }
-            Console.WriteLine("Матрица 7x4:");
-            for (int i = 0; i < 7; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    Console.Write(матрица[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("\nСтроки, содержащие хотя бы один нечетный отрицательный элемент:");
-            for (int i = 0; i < 7; i++)
-            {
-                bool естьНечетныйОтрицательный = false;
-                for (int j = 0; j < 4; j++)
-                {
-                    if (матрица[i, j] < 0 && матрица[i, j] % 2 != 0)
-                    {
-                        естьНечетныйОтрицательный = true;
-                        break;
-                    }
-                }
-                if (естьНечетныйОтрицательный)
-                {
-                    Console.Write((i + 1) + " ");
-                }
-            }
-            Console.ReadKey();
         }
+        Console.WriteLine("Матрица 7x4:");
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                Console.Write(матрица[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("\nСтроки, содержащие хотя бы один нечетный отрицательный элемент:");
+        for (int i = 0; i < 7; i++)
+        {
+            bool естьНечетныйОтрицательный = false;
+            for (int j = 0; j < 4; j++)
+            {
+                if (матрица[i, j] < 0 && матрица[i, j] % 2 != 0)
+                {
+                    естьНечетныйОтрицательный = true;
+                    break;
+                }
+            }
+            if (естьНечетныйОтрицательный)
+            {
+                Console.Write((i + 1) + " ");
+            }
+        }
+        Console.ReadKey();
     }
 }
