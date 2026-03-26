f7277d52c3ea47556462f19ab56af39c807d82c6
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:38:46 +0400

    Update Проект 7,3.cs

--- Проект 7,3.cs
+++ Проект 7,3.cs
@@ -1,42 +1,40 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Random rnd = new Random();
+        int[,] матрица = new int[15, 9];
+
+        for (int i = 0; i < 15; i++)
         {
-            Random rnd = new Random();
-            int[,] матрица = new int[15, 9];
-            
-            for (int i = 0; i < 15; i++)
+            for (int j = 0; j < 9; j++)
             {
-                for (int j = 0; j < 9; j++)
-                {
-                    матрица[i, j] = rnd.Next(-100, 101);
-                }
+                матрица[i, j] = rnd.Next(-100, 101);
             }
-            Console.WriteLine("Матрица 15x9:");
-            for (int i = 0; i < 15; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    Console.Write(матрица[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("\nМинимальные элементы в каждой строке:");
-            for (int i = 0; i < 15; i++)
-            {
-                int мин = матрица[i, 0];
-                for (int j = 1; j < 9; j++)
-                {
-                    if (матрица[i, j] < мин)
-                    {
-                        мин = матрица[i, j];
-                    }
-                }
-                Console.WriteLine($"Строка {i + 1}: {мин}");
-            }
-            Console.ReadKey();
         }
+        Console.WriteLine("Матрица 15x9:");
+        for (int i = 0; i < 15; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                Console.Write(матрица[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("\nМинимальные элементы в каждой строке:");
+        for (int i = 0; i < 15; i++)
+        {
+            int мин = матрица[i, 0];
+            for (int j = 1; j < 9; j++)
+            {
+                if (матрица[i, j] < мин)
+                {
+                    мин = матрица[i, j];
+                }
+            }
+            Console.WriteLine($"Строка {i + 1}: {мин}");
+        }
+        Console.ReadKey();
     }
 }
