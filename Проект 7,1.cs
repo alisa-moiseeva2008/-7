f42ec8e8f777442a52e6edc61f4e1884e0904091
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:30:33 +0400

    Update Проект 7,1.cs

--- Проект 7,1.cs
+++ Проект 7,1.cs
@@ -1,44 +1,42 @@
 using System;
+class Program
 {
-    class Program
+    static int[,] СоздатьМассив(int строки, int столбцы, int A, int B)
     {
-        static int[,] СоздатьМассив(int строки, int столбцы, int A, int B)
+        Random rnd = new Random();
+        int[,] массив = new int[строки, столбцы];
+
+        for (int i = 0; i < строки; i++)
         {
-            Random rnd = new Random();
-            int[,] массив = new int[строки, столбцы];
-            
-            for (int i = 0; i < строки; i++)
+            for (int j = 0; j < столбцы; j++)
             {
-                for (int j = 0; j < столбцы; j++)
-                {
-                    массив[i, j] = rnd.Next(A, B + 1);
-                }
+                массив[i, j] = rnd.Next(A, B + 1);
             }
-            return массив;
         }
-        static void Main(string[] args)
+        return массив;
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество строк: ");
+        int строки = int.Parse(Console.ReadLine());
+        Console.Write("Введите количество столбцов: ");
+        int столбцы = int.Parse(Console.ReadLine());
+        Console.Write("Введите A: ");
+        int A = int.Parse(Console.ReadLine());
+        Console.Write("Введите B: ");
+        int B = int.Parse(Console.ReadLine());
+
+        int[,] массив = СоздатьМассив(строки, столбцы, A, B);
+
+        Console.WriteLine("Сгенерированный массив:");
+        for (int i = 0; i < строки; i++)
         {
-            Console.Write("Введите количество строк: ");
-            int строки = int.Parse(Console.ReadLine());
-            Console.Write("Введите количество столбцов: ");
-            int столбцы = int.Parse(Console.ReadLine());
-            Console.Write("Введите A: ");
-            int A = int.Parse(Console.ReadLine());
-            Console.Write("Введите B: ");
-            int B = int.Parse(Console.ReadLine());
-            
-            int[,] массив = СоздатьМассив(строки, столбцы, A, B);
-            
-            Console.WriteLine("Сгенерированный массив:");
-            for (int i = 0; i < строки; i++)
+            for (int j = 0; j < столбцы; j++)
             {
-                for (int j = 0; j < столбцы; j++)
-                {
-                    Console.Write(массив[i, j] + "\t");
-                }
-                Console.WriteLine();
+                Console.Write(массив[i, j] + "\t");
             }
-            Console.ReadKey();
+            Console.WriteLine();
         }
+        Console.ReadKey();
     }
 }
