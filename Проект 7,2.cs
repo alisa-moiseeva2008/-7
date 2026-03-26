c2a7e226096704a2c17621e0efd4c2b683e739d4
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:37:02 +0400

    Update Проект 7,2.cs

--- Проект 7,2.cs
+++ Проект 7,2.cs
@@ -1,35 +1,32 @@
-using System;
+class Program
 {
-    class Program
+    static void ВывестиМассив(int[] массив)
     {
-        static void ВывестиМассив(int[] массив)
+        foreach (int элемент in массив)
         {
-            foreach (int элемент in массив)
+            Console.Write(элемент + " ");
+        }
+        Console.WriteLine();
+    }
+    static void ВывестиМассив(int[,] массив)
+    {
+        for (int i = 0; i < массив.GetLength(0); i++)
+        {
+            for (int j = 0; j < массив.GetLength(1); j++)
             {
-                Console.Write(элемент + " ");
+                Console.Write(массив[i, j] + "\t");
             }
             Console.WriteLine();
-        }     
-        static void ВывестиМассив(int[,] массив)
-        {
-            for (int i = 0; i < массив.GetLength(0); i++)
-            {
-                for (int j = 0; j < массив.GetLength(1); j++)
-                {
-                    Console.Write(массив[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
         }
-        static void Main(string[] args)
-        {
-            int[] одномерный = { 1, 2, 3, 4, 5 };
-            int[,] двумерный = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }; 
-            Console.WriteLine("Одномерный массив:");
-            ВывестиМассив(одномерный);
-            Console.WriteLine("Двумерный массив:");
-            ВывестиМассив(двумерный);
-            Console.ReadKey();
-        }
+    }
+    static void Main(string[] args)
+    {
+        int[] одномерный = { 1, 2, 3, 4, 5 };
+        int[,] двумерный = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        Console.WriteLine("Одномерный массив:");
+        ВывестиМассив(одномерный);
+        Console.WriteLine("Двумерный массив:");
+        ВывестиМассив(двумерный);
+        Console.ReadKey();
     }
 }
