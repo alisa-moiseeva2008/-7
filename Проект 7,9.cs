65e62aa8a38c2975a45f1aac4bd0bc05e96c5395
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:42:29 +0400

    Update Проект 7,9.cs

--- Проект 7,9.cs
+++ Проект 7,9.cs
@@ -1,61 +1,59 @@
 using System;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Введите размер матрицы N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        Random rnd = new Random();
+        int[,] матрица = new int[N, N];
+
+        for (int i = 0; i < N; i++)
         {
-            Console.Write("Введите размер матрицы N: ");
-            int N = int.Parse(Console.ReadLine());
-            
-            Random rnd = new Random();
-            int[,] матрица = new int[N, N];
-            
-            for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
             {
-                for (int j = 0; j < N; j++)
-                {
-                    матрица[i, j] = rnd.Next(-100, 101);
-                }
+                матрица[i, j] = rnd.Next(-100, 101);
             }
-            Console.WriteLine("Исходная матрица:");
-            for (int i = 0; i < N; i++)
-            {
-                for (int j = 0; j < N; j++)
-                {
-                    Console.Write(матрица[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
-            int макс = матрица[0, 0];
-            int мин = матрица[0, 0];
-            int индексМакс = 0, индексМин = 0;
-            
-            for (int i = 1; i < N; i++)
-            {
-                if (матрица[i, i] > макс)
-                {
-                    макс = матрица[i, i];
-                    индексМакс = i;
-                }
-                if (матрица[i, i] < мин)
-                {
-                    мин = матрица[i, i];
-                    индексМин = i;
-                }
-            }
-            int temp = матрица[индексМакс, индексМакс];
-            матрица[индексМакс, индексМакс] = матрица[индексМин, индексМин];
-            матрица[индексМин, индексМин] = temp;
-            Console.WriteLine("\nМатрица после обмена:");
-            for (int i = 0; i < N; i++)
-            {
-                for (int j = 0; j < N; j++)
-                {
-                    Console.Write(матрица[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
-            Console.ReadKey();
         }
+        Console.WriteLine("Исходная матрица:");
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                Console.Write(матрица[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        int макс = матрица[0, 0];
+        int мин = матрица[0, 0];
+        int индексМакс = 0, индексМин = 0;
+
+        for (int i = 1; i < N; i++)
+        {
+            if (матрица[i, i] > макс)
+            {
+                макс = матрица[i, i];
+                индексМакс = i;
+            }
+            if (матрица[i, i] < мин)
+            {
+                мин = матрица[i, i];
+                индексМин = i;
+            }
+        }
+        int temp = матрица[индексМакс, индексМакс];
+        матрица[индексМакс, индексМакс] = матрица[индексМин, индексМин];
+        матрица[индексМин, индексМин] = temp;
+        Console.WriteLine("\nМатрица после обмена:");
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                Console.Write(матрица[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
     }
 }
